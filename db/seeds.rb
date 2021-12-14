c297cb3cd0d6b4b4c1a85de9d64e128c1fdf30bb
Casey <cantrellcasey@gmail.com>
2021-12-13 20:51:12 -0600

    seeds db from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('lib/nasdaq_screener.csv'), headers: true) do |row|
+  
+  Stock.create({
+    symbol: row[0],
+    name: row[1],
+    last_sale: row[2],
+    net_change: row[3],
+    percent_change: row[4],
+    market_cap: row[5],
+    country: row[6],
+    ipo_year: row[7],
+    volume: row[8],
+    sector: row[9],
+    industry: row[10]
+  })
+end
