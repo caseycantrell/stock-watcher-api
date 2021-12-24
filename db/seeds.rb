b0d86820a53d2272d0fa1995ce2f9b4cf6eedeb4
Casey <cantrellcasey@gmail.com>
2021-12-24 14:42:07 -0600

    comments out seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-require 'csv'
+# require 'csv'
 
-CSV.foreach(Rails.root.join('lib/nasdaq_screener.csv'), headers: true) do |row|
+# CSV.foreach(Rails.root.join('lib/nasdaq_screener.csv'), headers: true) do |row|
   
-  Stock.create({
-    symbol: row[0],
-    name: row[1],
-    last_sale: row[2],
-    net_change: row[3],
-    percent_change: row[4],
-    market_cap: row[5],
-    country: row[6],
-    ipo_year: row[7],
-    volume: row[8],
-    sector: row[9],
-    industry: row[10]
-  })
-end
+#   Stock.create({
+#     symbol: row[0],
+#     name: row[1],
+#     last_sale: row[2],
+#     net_change: row[3],
+#     percent_change: row[4],
+#     market_cap: row[5],
+#     country: row[6],
+#     ipo_year: row[7],
+#     volume: row[8],
+#     sector: row[9],
+#     industry: row[10]
+#   })
+# end
